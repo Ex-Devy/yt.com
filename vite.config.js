2dbd4fab809d746ae96dc2b30383b86ad872d9d2
Ex-Devy <miguel.henrique.roveri.bueno@gmail.com>
2023-09-16 21:32:27 -0300

    first commit

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,7 @@ import { resolve } from "path";
 import { defineConfig } from "vite";
 
 export default defineConfig({
-  base: "/yt2.com/",
+  base: "/yt.com/",
   plugins: [],
   resolve: {
     /*something*/
